48b7b0e2440fe7e62da29b7ce1d716e64ba395ea
Felipe Oliveira Carvalho <felipekde@gmail.com>
2014-07-23 20:09:08 -0300

    Bump version to 0.2.2

--- js/jsx.js
+++ js/jsx.js
@@ -28,7 +28,7 @@ define(['JSXTransformer', 'text'], function (JSXTransformer, text) {
   var buildMap = {};
 
   var jsx = {
-    version: '0.2.1',
+    version: '0.2.2',
 
     load: function (name, req, onLoadNative, config) {
       var fileExtension = config.jsx && config.jsx.fileExtension || '.js';
